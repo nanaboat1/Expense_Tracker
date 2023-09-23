c7ff47e6a2783b9fc8999ed420273c7057c6ca46
Nana Boateng Amoah <amoahnanaboateng@yahoo.com>
2023-09-22 22:29:24 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace expense_tracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
