8e93c452a6e1ab37ad5901a4e78e0c69687b7512
Nana Boateng Amoah <amoahnanaboateng@yahoo.com>
2023-10-08 06:43:35 -0400

    database working; adding styles to create category page.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace expense_tracker.Models
+{
+	public class AppDbContext : DbContext 
+	{
+		public DbSet<Transaction> Transactions { get; set; }
+		public DbSet<Category> Categories { get; set; }
+
+		public AppDbContext(DbContextOptions options) : base (options)
+		{
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,24 +1,50 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using expense_tracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql; 
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        //
+        builder.Services.AddScoped<AppDbContext>(); 
+
+        // Dependency Injection
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContextPool<AppDbContext>(options =>
+        {
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        });
+
+        // Syncfusion licence key
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1NpRGZGfV5yd0VHYFZSQnxcQE0SNHVRdkdgWH9eeHVVRWZfU0B3W0M=");
+
+        // Build app
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-
